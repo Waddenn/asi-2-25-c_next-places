f5d49377e071095a52c4301630f387968ca7f283
waddenn <waddenn.github@gmail.com>
2024-05-02 12:53:46 +0200

    address list component added

---
+++ src/components/AddressList/AddressList.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import styles from "./AddressList.module.css";
+import AddressItem from "./AddressItem";
+import {
+  FaMapMarkerAlt,
+  FaUtensils,
+  FaGlassMartiniAlt,
+  FaTree,
+  FaLandmark,
+} from "react-icons/fa";
+import useFetchData from "../../hooks/useFetchData";
+
+const typeIcons = {
+  restaurant: FaUtensils,
+  museum: FaLandmark, // Changed 'musee' to 'museum'
+  bar: FaGlassMartiniAlt,
+  park: FaTree, // Changed 'parc' to 'park'
+};
+
+const AddressList = ({ filters }) => {
+  const places = useFetchData(filters);
+  const [visibleItems, setVisibleItems] = useState([]);
+
+  useEffect(() => {
+    setVisibleItems([]);
+    const timer = setTimeout(() => {
+      setVisibleItems(places);
+    }, 0);
+
+    return () => clearTimeout(timer);
+  }, [places]);
+
+  return (
+    <ul className={styles.listContainer}>
+      {visibleItems.map((place) => {
+        const TypeIcon = typeIcons[place.placeType] || FaMapMarkerAlt; 
+        return (
+          <AddressItem key={place._id} place={place} TypeIcon={TypeIcon} />
+        ); 
+      })}
+    </ul>
+  );
+};
+
+export default AddressList;
