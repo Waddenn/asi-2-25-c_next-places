2572402c80bae8125258da45453a570f30fc075d
waddenn <waddenn.github@gmail.com>
2024-05-04 21:48:42 +0200

    typeicons constant

---
+++ src/constants/typeIcons.js
@@ -0,0 +1,14 @@
+import {
+  FaUtensils,
+  FaGlassMartiniAlt,
+  FaTree,
+  FaLandmark,
+} from "react-icons/fa";
+
+const typeIcons = {
+  restaurant: FaUtensils,
+  museum: FaLandmark,
+  bar: FaGlassMartiniAlt,
+  park: FaTree,
+};
+export default typeIcons;
