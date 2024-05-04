689f738600b08ebc4445bf04008d8f767271789e
waddenn <waddenn.github@gmail.com>
2024-05-04 21:50:56 +0200

    import userformvalidation

--- src/pages/add/index.js
+++ src/pages/add/index.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { usePlaceData } from "@/hooks/usePlaceData";
 import Layout from "@/components/Layout/Layout";
 import Form from "@/components/Form/Form";
 import Select from "@/components/Form/Select";
 import initialFormData from "@/constants/formInitialValues";
+import useFormValidation from "@/hooks/useFormValidation";
 
 export default function AddPlace() {
   const router = useRouter();
-  const { formData, handleInputChange } = usePlaceData(null, initialFormData);
+  const { formData, errors, handleInputChange, validateForm } =
+    useFormValidation(initialFormData);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
+
     const response = await fetch("/api/places", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
     });
+
     if (response.ok) {
       router.push("/");
     } else {
@@ -28,7 +34,7 @@ export default function AddPlace() {
     <Layout>
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col w-2/5 mx-auto p-8 bg-white shadow-md border border-gray-300 rounded-lg"
+        className="relative z-10 flex flex-col w-2/5 mx-auto p-8 bg-white shadow-md border border-gray-300 rounded-lg"
       >
         <Select
           label="Place Type"
@@ -36,12 +42,17 @@ export default function AddPlace() {
           options={["restaurant", "museum", "bar", "park"]}
           value={formData.placeType}
           onChange={handleInputChange}
+          errorMessage={errors.placeType}
           className="mb-4"
         />
-        <Form formData={formData} handleInputChange={handleInputChange} />
+        <Form
+          formData={formData}
+          handleInputChange={handleInputChange}
+          errors={errors}
+        />
         <button
           type="submit"
-          className="py-3 px-6 bg-gray-800 text-white rounded-md mt-4 hover:bg-gray-900 transition-colors uppercase tracking-wide"
+          className="py-3 px-6 bg-[rgb(44,62,80)] text-white rounded-md mt-4 hover:bg-[rgb(54,72,90)] transition-colors uppercase tracking-wide"
         >
           Add
         </button>
