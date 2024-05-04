5fee47ad99d4bc22e7c91bd5dcf73b77bc1245d6
waddenn <waddenn.github@gmail.com>
2024-05-04 21:47:14 +0200

    new component textinput

---
+++ src/components/Form/TextInput.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const TextInput = ({
+  label,
+  name,
+  type = "text",
+  value,
+  onChange,
+  errorMessage,
+  ...props
+}) => (
+  <div>
+    <label htmlFor={name} className="block text-gray-700 font-bold mb-2">
+      {label}
+    </label>
+    <input
+      type={type}
+      id={name}
+      name={name}
+      value={value}
+      onChange={onChange}
+      className={`appearance-none block w-full bg-white text-gray-700 border ${errorMessage ? "border-red-500" : "border-gray-400"} rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500`}
+      {...props}
+    />
+    {errorMessage && (
+      <p className="text-red-500 text-xs italic">{errorMessage}</p>
+    )}
+  </div>
+);
+
+export default TextInput;
