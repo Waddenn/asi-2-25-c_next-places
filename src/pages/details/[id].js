acae377a2c4ff479d17121fa2da1f501ca4436c9
waddenn <waddenn.github@gmail.com>
2024-05-04 21:50:27 +0200

    import userformvalidation

--- src/pages/details/[id].js
+++ src/pages/details/[id].js
@@ -1,15 +1,32 @@
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-import { usePlaceData } from "@/hooks/usePlaceData";
 import Layout from "@/components/Layout/Layout";
 import Form from "@/components/Form/Form";
+import useFormValidation from "@/hooks/useFormValidation";
 import initialFormData from "@/constants/formInitialValues";
 
 const PlaceDetails = ({ id }) => {
   const router = useRouter();
-  const { formData, handleInputChange } = usePlaceData(id, initialFormData);
+  const { formData, errors, handleInputChange, setFormData, validateForm } =
+    useFormValidation(initialFormData);
+
+  useEffect(() => {
+    const loadPlaceData = async () => {
+      const response = await fetch(`/api/places/?id=${id}`);
+      if (response.ok) {
+        const data = await response.json();
+        setFormData(data);
+      }
+    };
+
+    loadPlaceData();
+  }, [id, setFormData]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
     const response = await fetch(`/api/places/?id=${id}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
@@ -33,7 +50,11 @@ const PlaceDetails = ({ id }) => {
         onSubmit={handleSubmit}
         className="flex flex-col w-2/5 mx-auto p-8 bg-white shadow-md border border-gray-300 rounded-lg mb-10"
       >
-        <Form formData={formData} handleInputChange={handleInputChange} />
+        <Form
+          formData={formData}
+          handleInputChange={handleInputChange}
+          errors={errors}
+        />
         <div className="flex justify-evenly items-center mt-4">
           <button
             type="button"
